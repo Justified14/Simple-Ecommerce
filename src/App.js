93740949ae9993fff3041b9e216a7935308acfbd
Justified14 <Justified14>
2023-05-04 17:56:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavHead from './component/NavHead';
+import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home'
+import Footer from './component/Footer';
+import Product from './Pages/Product';
+import Productid from './Pages/Productid';
+import Category from './Pages/Category';
+import CategoryId from './Pages/CategoryId'
+import Search from './Pages/Search';
+import { StateContext } from './Lib/ContextApi';
+import { Toaster } from 'react-hot-toast';
+import Cart from './Pages/Cart';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <StateContext>
+      <Toaster/>
+     <NavHead/> 
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="product" element={<Product/>}>
+        <Route path=":productid" element={<Productid/>} />
+        </Route>
+        <Route path="categories" element={<Category/>}>
+        <Route path=":categoryid" element={<CategoryId/>} />
+        </Route>
+        <Route path="search" element={<Search/>} />
+        <Route path="cart" element={<Cart/>} />
+     </Routes>
+     <Footer/>
+     </StateContext>
+    </>
   );
 }
 
